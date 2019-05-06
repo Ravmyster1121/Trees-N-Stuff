37ac049ed58f1ce2173afb89ae25526a92aeeec2
Ravmyster1121 <rhninety@gmail.com>
2019-05-06 20:13:20 +1200

    Changed from Stencil to Angular
    Changed away from stencil

--- stencil.config.ts
+++
@@ -1,9 +0,0 @@
-import { Config } from '@stencil/core';
-
-// https://stenciljs.com/docs/config
-
-export const config: Config = {
-  outputTargets: [{ type: 'www' }],
-  globalScript: 'src/global/app.ts',
-  globalStyle: 'src/global/app.css'
-};
