03919044b0ffe3f796c17e2446541f2b30bc9a8b
LordAntonidus <42327259+LordAntonidus@users.noreply.github.com>
2019-05-13 22:55:57 +1200

    Attempting to add more views - Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Angular router import for allowing more than "one page/view"
+import { RouterModule, Routes } from '@angular/router';
+
+/*
+const appRoutes: Routes = [
+  { path: 'crisis-center',    component: CrisisListComponent },
+  { path: 'hero/:id',         component: HeroDetailComponent},
+  {
+    path: 'heroes',
+    component: HeroListComponent,
+    data: { title: 'Heroes List'}
+  }
+];
+*/
+
 import { AppComponent } from './app.component';
 
 @NgModule({
