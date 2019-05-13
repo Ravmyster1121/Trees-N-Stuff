c14ab5a8f8f9861fc9f71cf4f9079f544453dd3c
LordAntonidus <42327259+LordAntonidus@users.noreply.github.com>
2019-05-14 00:34:20 +1200

    Manually Reverted Attempt to add more views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-// Angular router import for allowing more than "one page/view"
-import { RouterModule, Routes } from '@angular/router';
-
-/*
-const appRoutes: Routes = [
-  { path: 'crisis-center',    component: CrisisListComponent },
-  { path: 'hero/:id',         component: HeroDetailComponent},
-  {
-    path: 'heroes',
-    component: HeroListComponent,
-    data: { title: 'Heroes List'}
-  }
-];
-*/
-
 import { AppComponent } from './app.component';
 
 @NgModule({
