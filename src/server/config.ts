d5ea82aaf69515b7cbe5dff22182faea5dfc7133
Ravmyster1121 <rhninety@gmail.com>
2019-05-14 23:21:07 +1200

    Revert "following tutorial on https://medium.com/@micklynch_6905/angular-6-a-simple-web-application-with-node-and-postgresql-ee2a562887d6"
    This reverts commit 2ec35e347d728691f52525b05f3d87bd51311c6d.

--- src/server/config.ts
+++
@@ -1,9 +0,0 @@
-export const dbconfig =
-{
-    username : "tvkksylpgwoxtx",
-    password : "70d31e92d0f20119e45c6ffc02d3014d8d20fa3a6dfd2daf8e8ff42ff8d3d096",
-    database : "d10iibe6j1pc7c",
-    host : "ec2-54-163-226-238.compute-1.amazonaws.com",
-    dialect : "postgres",
-    port : 5432
-}
