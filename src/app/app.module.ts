a525f1e8627cc88ec7058758a0002dc11ff9e54a
LordAntonidus <42327259+LordAntonidus@users.noreply.github.com>
2019-05-14 15:43:23 +1200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
+// These Components were made by running the command "ng generate component component-name" and will appear here
+import { CartViewComponent }      from './cart-view/cart-view.component';
+import { TreeListComponent }      from './tree-list/tree-list.component';
+import { PageNotFoundComponent }  from './page-not-found/page-not-found.component';
+
+import { FormsModule }            from '@angular/forms';
+import { RouterModule, Routes }   from '@angular/router';
+
+// You will need to add these routes manually
+const appRoutes: Routes = [
+  { path: 'cart-view', component: CartViewComponent},
+  { path: 'tree-list', component: TreeListComponent},
+  { path: '', redirectTo: '/tree-list', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent},
+]
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartViewComponent,
+    TreeListComponent,
+    PageNotFoundComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
