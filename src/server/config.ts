b9822f60094e3a27204444bda9564c76596fb66c
Ravmyster1121 <rhninety@gmail.com>
2019-05-14 21:38:58 +1200

    Revert "following the tutorial"
    This reverts commit e58565dc7005e1597671a612826f7c1b2761af51.

--- src/server/config.ts
+++
@@ -1,9 +0,0 @@
-export const dbconfig =
-{
-    username : "tvkksylpgwoxtx",
-    password : "postgres",
-    database : "70d31e92d0f20119e45c6ffc02d3014d8d20fa3a6dfd2daf8e8ff42ff8d3d096",
-    host : "ec2-54-163-226-238.compute-1.amazonaws.com",
-    dialect : "postgres",
-    port : 5432
-}
