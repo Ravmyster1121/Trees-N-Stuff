f65db9795cfa5cb2c55b86822d3f4486d194346a
Ravmyster1121 (Laptop) <rhninety@gmail.com>
2019-05-21 13:45:28 +1200

    Images are now working

--- src/app/tree-list/tree-list.component.ts
+++ src/app/tree-list/tree-list.component.ts
@@ -12,21 +12,24 @@ export class TreeListComponent {
       name: "Oak Sapling",
       isTree: true,
       itemPrice: 25,
-      itemDescr: "Use on dirt to plant sapling."
+      itemDescr: "Use on dirt to plant sapling.",
+      itemImg: "tree.jpg"
     },
     {
       id: 2,
       name: "Hoe",
       isTree: false,
       itemPrice: 10,
-      itemDescr: "Use on dirt to till into farmland."
+      itemDescr: "Use on dirt to till into farmland.",
+      itemImg: "hoe.png"
     },
     {
       id: 3,
       name: "Axe",
       isTree: false,
       itemPrice: 10,
-      itemDescr: "Chop wood faster when using an axe."
+      itemDescr: "Chop wood faster when using an axe.",
+      itemImg: "axe.png"
     }
   ];
 }
