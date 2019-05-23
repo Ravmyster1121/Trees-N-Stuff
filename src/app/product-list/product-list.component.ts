d001eaae5a3f9c2c0634035c5892e8de089a9fa4
NghtFrost107 <hbn8839@autuni.ac.nz>
2019-05-24 00:12:25 +1200

    Can create the cards for the search
    cant get the information from the product array and put it into the cards

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,7 +8,7 @@ import { PRODUCTS } from '../products-data';
 })
 export class ProductListComponent {
   storeItems = PRODUCTS;
-
+  
   prevSearch = [];
 
   searchProduct(userSearch: string) {
@@ -19,4 +19,23 @@ export class ProductListComponent {
       }   
     }
   }
+  
+  productsFound = [];
+
+  findProduct(userSearch: string) {
+    for(var i = 0; i <= PRODUCTS.length; i++){
+      if (PRODUCTS.filter(p => p.name == userSearch))
+      {
+        this.productsFound.push(PRODUCTS[i]);
+      }
+    }
+    
+    this.storeItems = this.productsFound
+  }
+
+  // var productId = parseInt(this.route.snapshot.paramMap.get('id'));
+  // this.porductsFound[i] = new Products(PRODUCTS[productId].id, PRODUCTS[productId].name, PRODUCTS[productId].price, 
+  //   PRODUCTS[productId].cardDescr, PRODUCTS[productId].pageDescr, PRODUCTS[productId].itemType, PRODUCTS[productId].thumbnail, 
+  //   PRODUCTS[productId].img1, PRODUCTS[productId].img2, PRODUCTS[productId].img3)
+
 }
